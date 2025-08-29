fcc86ad618827f8fda68462d4b5bdaf2f2ca5ae8
Pradipsinh <mrvaghela2201@gmail.com>
2025-08-29 16:03:53 +0530

    Edit Main.py , remove unnecessary code.

--- main.py
+++ main.py
@@ -1,22 +1,11 @@
 from turtle import Turtle, Screen, colormode
 import random
-# import colorgram
 
 colormode(255)
 tim = Turtle()
 tim.speed("fastest")
 tim.hideturtle()
 print(tim)
-
-# rgb_colors = []
-# colors = colorgram.extract('Image_1.jpg', 20)
-#
-# for color in colors:
-#     r = color.rgb.r
-#     g = color.rgb.g
-#     b = color.rgb.b
-#     new_color = (r, g, b,)
-#     rgb_colors.append(new_color)
 
 rgb_colors = [(236, 35, 108), (221, 231, 237), (145, 28, 66), (239, 75, 35), (7, 148, 95), (220, 171, 45),
               (183, 158, 47), (45, 191, 232), (28, 127, 194), (254, 223, 0), (125, 192, 78), (85, 27, 91),
@@ -32,8 +21,6 @@ for _ in range(10):
         tim.dot(20, color)
         tim.forward(50)
     y_axes += 50
-
-
 
 screen = Screen()
 screen.exitonclick()
